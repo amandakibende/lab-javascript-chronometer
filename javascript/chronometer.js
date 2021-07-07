824e6cda0e1b5281703deab76053e5bd845a628b
Amanda KIBENDE <contactproamanda@gmail.com>
2021-07-07 16:38:13 +0200

    Iteration 1 = > getSecond

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,18 +1,36 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
+
+
 
   start(callback) {
     // ... your code goes here
+    this.intervalId = setInterval (() => {
+      
+      this.currentTime;
+      this.currentTime++;
+    }, 1000);
+
   }
+
+
 
   getMinutes() {
     // ... your code goes here
+
+    return Math.floor(this.currentTime/60)
   }
+
+
 
   getSeconds() {
     // ... your code goes here
+    return Math.floor(this.currentTime%60)
+
   }
 
   computeTwoDigitNumber(value) {
